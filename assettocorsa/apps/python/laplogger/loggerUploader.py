4067f847a93f9dbe364365b4c276b1ddc307f4ea
svitdolenc <svit.dolenc@dewesoft.com>
2024-04-30 13:41:20 +0200

    making a google sheet integration

--- assettocorsa/apps/python/laplogger/loggerUploader.py
+++ assettocorsa/apps/python/laplogger/loggerUploader.py
@@ -12,28 +12,49 @@ class GoogleSheetDemo:
                       "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
         self.creds = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", self.scope)
         self.client = gspread.authorize(self.creds)
-        self.sheet = self.client.open("Python-google-sheets-demo").sheet1
+        self.sheet  = self.client.open("Python-google-sheets-demo")
+        self.woorksheet = None
+
+    def open_sheet(self, sheet_name):
+        worksheet_list = self.sheet.worksheets()
+        for worksheet in worksheet_list:
+            print(worksheet)
+            if worksheet.title == sheet_name:
+                self.woorksheet = worksheet
+                return
+        self.woorksheet = self.sheet.add_worksheet(title=sheet_name, rows="100", cols="20")
 
     def get_row(self, row_number):
-        return self.sheet.row_values(row_number)
+        return self.woorksheet.row_values(row_number)
 
     def add_row(self, row_data):
-        self.sheet.insert_row(row_data, 1)
+        self.woorksheet.insert_row(row_data, 1)
         print("The row has been added")
 
     def parse_and_add_rows(self, data_string):
         data_lines = data_string.split('\n')
+        car = None
+        track = None
+        config = None
+        fastest_lap = None
         for line in data_lines:
             if not line: continue  # skip empty lines
             if 'car:' in line or 'track:' in line or 'config:' in line:
                 key, value = line.split(': ')
-                if key == 'car' or key == 'track' or key == 'config':
-                    self.add_row([key, value])
+                if key == 'car':
+                    car = value
+                elif key == 'track':
+                    track = value
+                elif key == 'config':
+                    config = value
+                if car and track and config:
+                     self.open_sheet(car + '-' + track + '-' + config)
+                continue
             else:
                 line = line.replace('False', 'false')
                 line = line.replace('True', 'true')
                 lap_data = json.loads(line.replace("'", "\""))  # Converting to valid JSON format by replacing single quotes with double quotes
-                lap_time = lap_data['time'] / 1000.0
+                lap_time = lap_data['time']
                 invalidated = lap_data['invalidated']
                 lap_number = lap_data['lap']
                 splits = lap_data['splits']
